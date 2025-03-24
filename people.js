1b4b644212818082f783632b10401bbde2a43945
PierdomenicoPacilio <piero.pacilio@gmail.com>
2025-03-24 20:06:51 +0100

    feat: third function

--- people.js
+++ people.js
@@ -1,3 +1,7 @@
 const objName = require('./names.js');
 
-const hobbiesArray = require('./hobbies.js')
+const hobbiesArray = require('./hobbies.js');
+
+const people = () => { return { fullName: objName('matteo', 'rossi'), hobbies: hobbiesArray('calcio', 'giardinaggio', 'lettura')}; };
+
+console.log(people());
